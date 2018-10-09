a0300b82c4ca2190232ac558c18544867c1423de
Nuno Ferreira <nunogcferreira@gmail.com>
2018-10-09 21:24:38 +0100

    mentor revised solution. Let it crash, they say. Elixir is awesome

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -8,16 +8,8 @@ defmodule RNATranscription do
   end
 
   @spec fetch_rna_transcription(char) :: char
-  defp fetch_rna_transcription(char), do: map_fetch_default(@rna_map, char)
-
-  # Maybe overkill, but I like parameterizing all arguments and defining smaller functions from the "parent" function.
-  # This is my way of currying in Elixir without going through the hassle of rewriting currying functions
-  # I'll gladly take sugestions on how to approach currying in a more Haskell-y way. :D
-  @spec map_fetch_default(map, char) :: char
-  defp map_fetch_default(map, char) do
-    case Map.fetch(map, char) do
-      {:ok, val} -> val
-      _ -> char
-    end
+  defp fetch_rna_transcription(char) do
+    @rna_map
+    |> Map.get(char)
   end
 end
