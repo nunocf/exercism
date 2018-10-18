9664761533b82a5559d379a5d94dc648ca47827d
Nuno Ferreira <nunogcferreira@gmail.com>
2018-10-18 19:23:56 +0100

    completed strain exercise. That was cool!

---
+++ elixir/strain/strain.exs
@@ -0,0 +1,39 @@
+defmodule Strain do
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns true.
+
+  Do not use `Enum.filter`.
+  """
+  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep(list, fun) do
+    list
+    |> Enum.reduce([], &do_filter(&1, &2, fun, true))
+    |> Enum.reverse()
+  end
+
+  @doc """
+  Given a `list` of items and a function `fun`, return the list of items where
+  `fun` returns false.
+
+  Do not use `Enum.reject`.
+  """
+  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard(list, fun) do
+    list
+    |> Enum.reduce([], &do_filter(&1, &2, fun, false))
+    # reverse the list because we've been inserting at the head
+    |> Enum.reverse()
+  end
+
+  defp do_filter(val, acc, fun, keep) do
+    cond do
+      # inserts at the head, to keep things a bit more optimal
+      fun.(val) == keep ->
+        [val] ++ acc
+
+      true ->
+        acc
+    end
+  end
+end
